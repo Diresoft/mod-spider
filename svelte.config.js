44d2878a3bdb7c7202ffb5acfaac14aa9c20161f
Labrasones <Labrasones@users.noreply.github.com>
2023-02-15 12:36:23 -0800

    Update Tauri + SvelteKit configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,16 @@
-import staticAdapter	from "@sveltejs/adapter-static";
-import preprocess 		from "svelte-preprocess";
-import { resolve }		from 'node:path';
-import { SassAlias }	from "svelte-preprocess-sass-alias-import";
-
-/**
- * SCSS Path Alias
- */
-const _scss_alias = new SassAlias({
-	"$scss": './src/scss'
-})
+import staticAdapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		scss: {
-			importer:		[ _scss_alias.resolve.bind( _scss_alias ) ],
-			renderSync:		true,
-			outputStyle:	'expanded'
-		}
-	}),
-	kit: {
-		adapter:	staticAdapter(),
-		alias:		{
-			"$lib": resolve( './src/lib' )
-		},
-		
-	},
+  preprocess: [vitePreprocess()],
+  kit: {
+    adapter: staticAdapter(),
+	alias:	 {
+		"$lib":		'src/lib',
+		"$scss":	'src/scss'
+	}
+  },
 };
 
 export default config;
